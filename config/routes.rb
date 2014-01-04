10996b7d1b1af96c9d5f3a79b9a6339a62d78187
Sam Brook <brookyy@gmail.com>
2014-01-04 12:34:48 +1100

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  root :to => 'static_pages#home'
 
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  match '/signup',  to: 'users#new'
+
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+  
 
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+ 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
