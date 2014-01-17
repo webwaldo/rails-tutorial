dd69cab8436acf05a42552cf52a8147e1b06b9ad
Sam Brook <brookyy@gmail.com>
2014-01-18 09:41:23 +1100

    Finish the signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
-  def new
-  end
+
+
+	def show
+		@user = User.find(params[:id])
+		#raise params.inspect
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(params[:user])
+		if @user.save 
+			flash[:success] = "Welcome to the Sample App!"
+			redirect_to user_path(@user)
+		else
+			render 'new'
+		end
+	end
+
 end
